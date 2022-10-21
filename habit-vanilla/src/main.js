ef6951f93a284209b99057e540ac720a164bc4f8
CooKingBeast <f62810@yahoo.com>
2022-10-21 15:25:55 +0500

    feat: cheking\uncheking days and progress bar

--- habit-vanilla/src/main.js
+++ habit-vanilla/src/main.js
@@ -1,23 +1,28 @@
-const modalSelector = document.querySelector('.modal')
 const modalTriggerSelector = document.querySelector('.modal-trigger')
+const modalSelector = document.querySelector('.modal')
 const modalInputSelector = document.getElementById('modal_input')
 const modalButtonSelector = document.getElementById('modal_button')
-const habitsSectionSelector = document.querySelector('.koko')
-console.log(modalButtonSelector)
+const progressBarSelector = document.querySelector('.progress-bar')
+const habitsSectionSelector = document.querySelector('.habits-section')
+
+let totalDays = 0
+let checkedDays = 0
 
 const handleModal = () => {
 	if (modalSelector.classList.contains('modal-disable')) {
 		modalSelector.classList.remove('modal-disable')
 	}
-	// else modalSelector.classList.add('modal-disable')
 }
 
 const handleAddHabit = () => {
-	modalSelector.classList.add('modal-disable')
-	handleCreateHabit(modalInputSelector.value.trim())
+	if (modalInputSelector.value.trim()) {
+		modalSelector.classList.add('modal-disable')
+		handleCreateHabit(modalInputSelector.value.trim())
+	}
+	modalInputSelector.value = ''
 }
 
-const handleCreateHabit = (test = '') => {
+const handleCreateHabit = (habitName) => {
 	habitsSectionSelector.insertAdjacentHTML(
 		'afterbegin',
 		`
@@ -28,28 +33,11 @@ const handleCreateHabit = (test = '') => {
 								alt="lifestyle"
 								width="80"
 							/>
-							<span class="font-bold text-2xl">${test}</span>
+							<span class="font-bold text-2xl">${habitName}</span>
 						</div>
-
 						<div class="flex items-center gap-6">
-							<button
-								class="h-12 w-12 flex items-center justify-center"
-							>
-								<img
-									src="../assets/checked.png"
-									alt="check-mark"
-									width="50"
-								/>
-							</button>
-							<button
-								class="h-12 w-12 flex items-center justify-center"
-							>
-								<img
-									src="../assets/checked.png"
-									alt="check-mark"
-									width="50"
-								/>
-							</button>
+							<button class="habit-buttons">M</button>
+							<button class="habit-buttons">T</button>
 							<button class="habit-buttons">W</button>
 							<button class="habit-buttons">T</button>
 							<button class="habit-buttons">F</button>
@@ -58,7 +46,31 @@ const handleCreateHabit = (test = '') => {
 						</div>
 					</div>`
 	)
+	totalDays += 7
+	refreshProgressBar()
+}
+
+const handleChecked = (target) => {
+	if (target.classList.contains('habit-buttons')) {
+		target.classList.replace('habit-buttons', 'checked')
+		checkedDays += 1
+	} else {
+		target.classList.replace('checked', 'habit-buttons')
+		checkedDays -= 1
+	}
+	refreshProgressBar()
+}
+
+const refreshProgressBar = () => {
+	const progress = Math.round((checkedDays / totalDays) * 100)
+	progressBarSelector.style.width = `${progress}%`
+	progressBarSelector.innerText = `${progress}%`
 }
 
 modalTriggerSelector.addEventListener('click', handleModal)
 modalButtonSelector.addEventListener('click', handleAddHabit)
+habitsSectionSelector.addEventListener('click', (event) => {
+	if (event.target.tagName === 'BUTTON') {
+		handleChecked(event.target)
+	}
+})
